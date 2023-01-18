af32834c083fb2a56947ef3ebe109bab2fc329d9
Semir Selman <selmansemir555@gmail.com>
2023-01-18 15:20:28 +0100

    added portofolio tutorial project

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,19 @@
+import about from './about';
+import brands from './brands';
+import contact from './contact';
+import experiences from './experiences';
+import skills from './skills';
+import testemonials from './testemonials';
+import workExperience from './workExperience';
+import works from './works';
+
+export const schemaTypes = [
+    about,
+    brands,
+    contact,
+    experiences,
+    skills,
+    testemonials,
+    workExperience,
+    works
+]
